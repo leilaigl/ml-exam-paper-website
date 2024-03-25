0695b8e6c9eb1cbd93e41f317fc1c335e7f5fd4f
Neennanvarath <nneennanvarathh@gmail.com>
2024-03-26 00:23:47 +0800

    Update page.js

--- src/app/flash-cards/page.js
+++ src/app/flash-cards/page.js
@@ -12,13 +12,13 @@ export default function FlashCards() {
         <div className="flex justify-center space-x-4 mt-8">
           {/* Buttons moved to the bottom of the page */}
           <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">
-            Red Button
+            My answer is incorrect
           </button>
           <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
-            Green Button
+            I can do this question
           </button>
           <button className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50">
-            Grey Button
+            Skip
           </button>
         </div>
       </div>
